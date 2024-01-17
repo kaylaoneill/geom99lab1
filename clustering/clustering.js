4a76ffd0163d9a21fe1d4a3423194a317abf7b76
kaylaoneill <146447016+kaylaoneill@users.noreply.github.com>
2024-01-16 22:07:58 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -18,7 +18,7 @@ function initMap() {
   // Add a marker clusterer to manage the markers.
   new MarkerClusterer(map, markers, {
     imagePath:
-      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+      "/clustering/ontarioparkslist.csv",
   });
 }
 const locations = [
